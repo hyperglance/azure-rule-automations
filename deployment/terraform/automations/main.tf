c5fdae2456307e589aeea8f18ab22756196c44c4
Richard Creber <rcreber@hyperglance.com>
2021-09-30 10:42:18 +0100

    Refactor
    Introduce Role assignment and service id to limit scope
    
    Revert back to using func separately to deploy function
    
    General refactor of TF

--- deployment/terraform/automations/main.tf
+++ deployment/terraform/automations/main.tf
@@ -7,9 +7,9 @@ resource "random_pet" "hyperglance-automations-name" {
 
 # Storage account name can only consist of lowercase letters and numbers, and must be between 3 and 24 characters long
 resource "random_string" "hyperglance-automations-storage-account-name" {
-  length = 12
-  lower = true
-  upper = false
+  length  = 12
+  lower   = true
+  upper   = false
   special = false
 }
 
@@ -17,7 +17,7 @@ resource "random_string" "hyperglance-automations-storage-account-name" {
 resource "azurerm_resource_group" "hyperglance-automations-resource-group" {
   name     = random_pet.hyperglance-automations-name.id
   location = "East US"
-  tags = var.tags
+  tags     = var.tags
 }
 
 # Create storage account
@@ -27,8 +27,8 @@ resource "azurerm_storage_account" "hyperglance-automations-storage-account" {
   location                 = azurerm_resource_group.hyperglance-automations-resource-group.location
   account_tier             = "Standard"
   account_replication_type = "ZRS"
-  tags = var.tags
-  }
+  tags                     = var.tags
+}
 
 # Create a service plan for function to be assigned to
 resource "azurerm_app_service_plan" "hyperglance-automations-app-service-plan" {
@@ -36,7 +36,7 @@ resource "azurerm_app_service_plan" "hyperglance-automations-app-service-plan" {
   location            = azurerm_resource_group.hyperglance-automations-resource-group.location
   resource_group_name = azurerm_resource_group.hyperglance-automations-resource-group.name
   kind                = "FunctionApp"
-  reserved = true
+  reserved            = true
   sku {
     tier = "Dynamic"
     size = "Y1"
@@ -44,65 +44,63 @@ resource "azurerm_app_service_plan" "hyperglance-automations-app-service-plan" {
   tags = var.tags
 }
 
-# Create the function, assign it to service plan, link it to storage account and assign
-# WEBSITE_RUN_FROM_PACKAGE setting to load function from blob using SAS
+# Create the function, assign it to service plan and link it to storage account
 resource "azurerm_function_app" "hyperglance-automations-app" {
   name                       = random_pet.hyperglance-automations-name.id
   location                   = azurerm_resource_group.hyperglance-automations-resource-group.location
   resource_group_name        = azurerm_resource_group.hyperglance-automations-resource-group.name
-  app_service_plan_id        = azurerm_app_service_plan.hyperglance-automations-app-service-plan.id 
+  app_service_plan_id        = azurerm_app_service_plan.hyperglance-automations-app-service-plan.id
   storage_account_name       = azurerm_storage_account.hyperglance-automations-storage-account.name
   storage_account_access_key = azurerm_storage_account.hyperglance-automations-storage-account.primary_access_key
-  version = "~3"
-  os_type = "linux"
+  version                    = "~3"
+  os_type                    = "linux"
   identity {
-    type = "SystemAssigned"
+    type         = "SystemAssigned"
   }
   app_settings = {
-    "AzureWebJobsDisableHomepage" = true
-    "WEBSITE_RUN_FROM_PACKAGE"    = "https://${azurerm_storage_account.hyperglance-automations-storage-account.name}.blob.core.windows.net/${azurerm_storage_container.hyperglance-automations-storage-container.name}/${azurerm_storage_blob.hyperglance-automations-blob.name}${data.azurerm_storage_account_blob_container_sas.hyperglance-automations-blob-container-sas.sas}",
+    "hyperglanceautomations_STORAGE" = azurerm_storage_account.hyperglance-automations-storage-account.primary_connection_string
+    "AzureWebJobsDisableHomepage"    = true
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.hyperglance-automations-application-insights.instrumentation_key
+    "ENABLE_ORYX_BUILD" = true
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" = 1
+    "FUNCTIONS_WORKER_RUNTIME" = "python"
+    "BUILD_FLAGS" = "UseExpressBuild"
   }
   tags = var.tags
 }
 
-# Create storage container to store zip that is loaded by function
+# Enable application insights for function
+resource "azurerm_application_insights" "hyperglance-automations-application-insights" {
+  name                = random_pet.hyperglance-automations-name.id
+  location            = azurerm_resource_group.hyperglance-automations-resource-group.location
+  resource_group_name = azurerm_resource_group.hyperglance-automations-resource-group.name
+  application_type    = "other"
+}
+
+# Create storage container for Hyperglance function
 resource "azurerm_storage_container" "hyperglance-automations-storage-container" {
-  name                  = "${random_pet.hyperglance-automations-name.id}-releases"
+  name                  = "hyperglance-automations"
   storage_account_name  = azurerm_storage_account.hyperglance-automations-storage-account.name
   container_access_type = "private"
 }
 
-# Generate ZIP to ready for upload to storage container
-data "archive_file" "hyperglance-automations-file" {
-  type        = "zip"
-  source_dir  = "../../../"
-  output_path = "Hyperglance_Automations_Function.zip"
-  excludes    = [ "deployment" ]
+
+#### Permissions ####
+
+# Get current subscription ID
+data "azurerm_subscription" "primary" {
 }
 
-# Upload ZIP to storage container, to be ingested by function app
-resource "azurerm_storage_blob" "hyperglance-automations-blob" {
-  name = "${filesha256(data.archive_file.hyperglance-automations-file.output_path)}.zip"
-  storage_account_name = azurerm_storage_account.hyperglance-automations-storage-account.name
-  storage_container_name = azurerm_storage_container.hyperglance-automations-storage-container.name
-  type = "Block"
-  source = data.archive_file.hyperglance-automations-file.output_path
+# Give function access to write to storage account
+resource "azurerm_role_assignment" "hyperglance-automations-storage-blob-contributor" {
+  scope                = azurerm_storage_account.hyperglance-automations-storage-account.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_function_app.hyperglance-automations-app.identity.0.principal_id
 }
 
-# Generate SAS to enable function app to load ZIP from blob
-data "azurerm_storage_account_blob_container_sas" "hyperglance-automations-blob-container-sas" {
-  connection_string = azurerm_storage_account.hyperglance-automations-storage-account.primary_connection_string
-  container_name    = azurerm_storage_container.hyperglance-automations-storage-container.name
-
-  start = "2021-01-01T00:00:00Z"
-  expiry = timeadd(timestamp(), "87660h")
-
-  permissions {
-    read   = true
-    add    = false
-    create = false
-    write  = false
-    delete = false
-    list   = false
-  }
+# Give function access to control VMs in current subscription
+resource "azurerm_role_assignment" "hyperglance-automations-virtual-machine-contributor" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Virtual Machine Contributor"
+  principal_id         = azurerm_function_app.hyperglance-automations-app.identity.0.principal_id
 }
