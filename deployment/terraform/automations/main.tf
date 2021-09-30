c02aa214184bcd8981ac877bc4b41f9c0f8a5e1b
Richard Creber <rcreber@hyperglance.com>
2021-09-30 11:53:42 +0100

    Format + var

--- deployment/terraform/automations/main.tf
+++ deployment/terraform/automations/main.tf
@@ -16,7 +16,7 @@ resource "random_string" "hyperglance-automations-storage-account-name" {
 # Create resource group for deployment
 resource "azurerm_resource_group" "hyperglance-automations-resource-group" {
   name     = random_pet.hyperglance-automations-name.id
-  location = "East US"
+  location = var.region
   tags     = var.tags
 }
 
@@ -55,16 +55,16 @@ resource "azurerm_function_app" "hyperglance-automations-app" {
   version                    = "~3"
   os_type                    = "linux"
   identity {
-    type         = "SystemAssigned"
+    type = "SystemAssigned"
   }
   app_settings = {
     "hyperglanceautomations_STORAGE" = azurerm_storage_account.hyperglance-automations-storage-account.primary_connection_string
     "AzureWebJobsDisableHomepage"    = true
     "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.hyperglance-automations-application-insights.instrumentation_key
-    "ENABLE_ORYX_BUILD" = true
+    "ENABLE_ORYX_BUILD"              = true
     "SCM_DO_BUILD_DURING_DEPLOYMENT" = 1
-    "FUNCTIONS_WORKER_RUNTIME" = "python"
-    "BUILD_FLAGS" = "UseExpressBuild"
+    "FUNCTIONS_WORKER_RUNTIME"       = "python"
+    "BUILD_FLAGS"                    = "UseExpressBuild"
   }
   tags = var.tags
 }
