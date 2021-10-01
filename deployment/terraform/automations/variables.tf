b74500259c8a496944fc5b5f5dd27b8f648ca087
Richard HG <rcreber@hyperglance.com>
2021-10-01 14:18:26 +0100

    Introduce Terraform deployment

---
+++ deployment/terraform/automations/variables.tf
@@ -0,0 +1,25 @@
+# Tags to deploy against resources
+variable "tags" {
+  type        = map(string)
+  description = "Resource Tags to Apply"
+  default = {
+    Name        = "Hyperglance Automations"
+    Persistent  = "True"
+    Description = "Resources Required by Hyperglance Automations"
+    Help        = "https://support.hyperglance.com/"
+    Source      = "https://github.com/hyperglance/azure-rule-automations"
+  }
+}
+
+# Define the Azure region to deploy the resources in
+variable "region" {
+  type        = string
+  description = "Azure region to deploy resources in e.g. East US"
+}
+
+# Cap the number of workers that can be allocated to the function
+variable "app_scale_limit" {
+  type = number
+  description = "Maximum number of workers that can be allocated to the function"
+  default = 5
+}
--- hyperglance_automations/actions/vm_stop.py
+++ hyperglance_automations/actions/vm_stop.py
@@ -2,7 +2,7 @@ from azure.mgmt.compute import ComputeManagementClient
 
 def hyperglance_automation( credential, resource: dict, automation_params = ''):
     client = ComputeManagementClient(credential, resource['subscription']) # subscription id
-    client.virtual_machines.begin_power_off(resource['attributes']['Resource Group'], resource['attributes']['Computer Name']) # crendentials and client config to be added
+    client.virtual_machines.begin_power_off(resource['attributes']['Resource Group'], resource['name']) # crendentials and client config to be added
 
 def info() -> dict:
   INFO = {
