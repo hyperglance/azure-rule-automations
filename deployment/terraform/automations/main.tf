a87d619e4d0baf287a5e9178669ac8893615234a
Richard Creber <rcreber@hyperglance.com>
2021-10-01 09:20:48 +0100

    use path.module

--- deployment/terraform/automations/main.tf
+++ deployment/terraform/automations/main.tf
@@ -90,7 +90,7 @@ resource "azurerm_storage_blob" "hyperglance-automations-json-blob" {
   storage_account_name   = azurerm_storage_account.hyperglance-automations-storage-account.name
   storage_container_name = azurerm_storage_container.hyperglance-automations-storage-container.name
   type                   = "Block"
-  source                 = "../../../files/HyperglanceAutomations.json"
+  source                 = "${path.module}/../../../files/HyperglanceAutomations.json"
 }
 
 #### Permissions ####
