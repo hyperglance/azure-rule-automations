b24b93613b616e71e88aed6d8146c354344671d7
Jake <joneill@hyperglance.com>
2021-10-07 11:38:23 +0100

    fix encoding

--- deployment/metadata/compress_code.py
+++ deployment/metadata/compress_code.py
@@ -9,8 +9,8 @@ def zip_code(output: pathlib.Path, to_zip: pathlib.Path):
     shutil.make_archive(output, 'zip', to_zip)
     with open(output + '.zip',"rb") as f:
         bytes = f.read() 
-        return encoder.b64encode(hashlib.sha256(bytes).digest);
-    
+        return encoder.standard_b64encode(hashlib.sha256(bytes).digest()).decode('utf-8');
+
 function_path = pathlib.Path(__file__).parents[2].joinpath('hyperglance_automations')
 digest = zip_code(sys.argv[1], function_path)
 
