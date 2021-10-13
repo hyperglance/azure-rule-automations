0040c22fbee9711be1422c653f934b00f89fcc8a
Jake <joneill@hyperglance.com>
2021-10-13 14:27:24 +0100

    add filemd5 to terraform config

--- deployment/terraform/modules/hyperglance-automations/main.tf
+++ deployment/terraform/modules/hyperglance-automations/main.tf
@@ -94,7 +94,7 @@ resource "azurerm_storage_blob" "hyperglance-automations-json-blob" {
   storage_account_name   = azurerm_storage_account.hyperglance-automations-storage-account.name
   storage_container_name = azurerm_storage_container.hyperglance-automations-storage-container.name
   type                   = "Block"
-  source                 = "${path.module}/../../../../files/HyperglanceAutomations.json"
+  source                 = filemd5("${path.module}/../../../../files/HyperglanceAutomations.json")
 }
 
 locals {
