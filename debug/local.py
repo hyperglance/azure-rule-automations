22d0a798f033e99888457c5d190528be821d90e5
Jake O'Neill <joneill@hyperglance.com>
2021-11-04 11:40:43 +0000

    minor changes to local

--- debug/local.py
+++ debug/local.py
@@ -7,16 +7,16 @@ import os
 
 
 def main(path: str):
-    hyperglance_path = pathlib.Path(__file__).parents[1].joinpath('hyperglance_automations')
+    hyperglance_path = pathlib.Path(__file__).parents[1]
     sys.path.append(str(hyperglance_path.absolute()))
-    processing = importlib.import_module("processing")
+    processing = importlib.import_module("hyperglance_automations.processing")
     file = pathlib.Path(path)
     with open(file, 'r', encoding='utf-8') as instream:
         mock_blob = instream.read()
     payload = json.loads(mock_blob)
     outputs = []
     try:
-        processing.process_event(payload) 
+        processing.process_event(payload, outputs) 
     except Exception as e:
         print(e)
     
