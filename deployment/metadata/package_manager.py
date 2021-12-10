4fa3433083387027fd646241fc3a38d358096014
Jake O'Neill <joneill@hyperglance.com>
2021-12-10 11:54:50 +0000

    further package_manager impl

--- deployment/metadata/package_manager.py
+++ deployment/metadata/package_manager.py
@@ -2,25 +2,30 @@
 import json
 from pathlib import Path
 import requests
+import re
+from wheel_filename import parse_wheel_filename
 from distutils.version import StrictVersion
-
 
 def get_urls(package_name) -> list:
     url = f'https://pypi.org/pypi/{package_name}/json'
-    data = json.loads(requests.get(url).content)['info']
-    #for item in data: 
-    print(json.dumps(data))
-    #return data
+    data = json.loads(requests.get(url).content)['urls']
+    files = {url['filename']: url['url'] for url in data if url['filename'].endswith('.whl')}
+    parsed = []
+    for file in files:
+        parsed += parse_wheel_filename(file[0])
+    print(files)
 
 def get_requirements() -> list:
     requirements_file = Path(__file__).resolve().parents[2].joinpath('requirements.txt')
     dependencies = []
+    regex = re.compile("[\(;].*$")
     with open(requirements_file) as requirements:
         lines = requirements.readlines()
         lines = [line.replace('\n', '') for line in lines if not line.startswith('#') and not len(line) == 1]
     for line in lines:
         url = f'https://pypi.org/pypi/{line}/json'
         dependencies += json.loads(requests.get(url).content)['info']['requires_dist']
+    dependencies = [re.sub(regex, '', dependency) for dependency in dependencies] # remove version info
     return lines + dependencies
 
 def fetch_packages(package_urls: list):
@@ -28,7 +33,12 @@ def fetch_packages(package_urls: list):
         requests.get(url)
     pass
 
+class Package:
+    def __init__(self, name: str, version: str):
+        self.name = name
+        self.version = version
+
 
 if __name__ == '__main__':
-    print(get_requirements())
+    get_urls('cryptography')
     #fetch_packages(get_urls(get_requirements))
