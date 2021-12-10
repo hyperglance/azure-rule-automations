d559f735d9e61634c51874667cc8dbd1c5f27c1e
Jake O'Neill <joneill@hyperglance.com>
2021-12-10 10:41:18 +0000

    Revert "package-manager"
    This reverts commit 9edbfc931b4720f77139ea51777dcc71d3e332ba.

--- deployment/metadata/package_manager.py
+++ deployment/metadata/package_manager.py
@@ -4,24 +4,17 @@ from pathlib import Path
 import requests
 from distutils.version import StrictVersion
 
-
 def get_urls(package_name) -> list:
     url = f'https://pypi.org/pypi/{package_name}/json'
-    data = json.loads(requests.get(url).content)['info']
-    #for item in data: 
-    print(json.dumps(data))
-    #return data
+    data = json.loads(requests.get(url).content)['urls']
+    return data
 
 def get_requirements() -> list:
     requirements_file = Path(__file__).resolve().parents[2].joinpath('requirements.txt')
-    dependencies = []
     with open(requirements_file) as requirements:
         lines = requirements.readlines()
         lines = [line.replace('\n', '') for line in lines if not line.startswith('#') and not len(line) == 1]
-    for line in lines:
-        url = f'https://pypi.org/pypi/{line}/json'
-        dependencies += json.loads(requests.get(url).content)['info']['requires_dist']
-    return lines + dependencies
+    return lines
 
 def fetch_packages(package_urls: list):
     for url in package_urls:
