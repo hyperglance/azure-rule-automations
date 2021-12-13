5b52943aa3606222bf7c7733f0d471ca602dec4f
David Gill <dgill@hyperglance.com>
2021-12-13 17:14:18 +0000

    Add initial multiprocessing implementation

--- hyperglance_automations/processing.py
+++ hyperglance_automations/processing.py
@@ -9,6 +9,65 @@ import json
 
 logger = logging.getLogger()
 
+def worker(resources, automation_name, action_params):
+    # time limits so we don't run for longer than the Azure function execution limit
+    time_elapsed = 0.0
+    time_limit = get_time_limit()
+
+    # Report object to return
+    report = {
+        "processed": [],
+        "errored": [],
+        "critical_error": None
+    }
+
+    #  TODO on a subscription (per group of subscriptions) basis when resources from hyper backend
+    # 1. Have Environment user facing metadata
+    # 2. Are grouped per subscription
+    if('core.windows.net' in os.environ["hyperglanceautomations_STORAGE"]):
+        cloud = AZURE_PUBLIC_CLOUD
+    else:
+        cloud = AZURE_US_GOV_CLOUD
+    
+    # Environment variables (Function App -> Settings -> Configuration -> Application Settings) 
+    # {AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET}
+    # or identity (Function App -> Identity) must be used to authenticate.
+    credential = identity.DefaultAzureCredential(environment=cloud.endpoints.active_directory)
+
+    ## Dynamically load the module that will handle this automation
+    try:
+        automation_to_execute = importlib.import_module(
+            "".join(["hyperglance_automations.", "actions.", automation_name])
+        )
+    except Exception as e:
+        logger.info(e)
+        msg = "Unable to find or load an automation called: %s" % automation_name
+        report["critical_error"] = msg
+        return report
+
+    ## For each of Resource, execute the automation
+    for resource in resources:
+        if time_elapsed > time_limit:
+            logger.info("time limit exceeded for " + str(resource))
+            resource["error"] = \
+            "The time limit for the action has surpassed. Consider changing your function app service plan. https://docs.microsoft.com/en-us/azure/app-service/app-service-plan-manage"
+            automation['errored'].append(resource)
+            continue
+
+        before = perf_counter()
+        try:
+            automation_to_execute.hyperglance_automation(credential, resource, cloud, action_params)
+            report["processed"].append(resource)
+        except Exception as err:
+            logger.info(err)
+            resource["error"] = str(err)  # augment resource with an error field
+            report["errored"].append(resource)
+        finally:
+            time_elapsed += (perf_counter()-before)
+    
+    return report
+
+
 def process_event(automation_data, outputs):
     time_elapsed = 0.0
     time_limit = get_time_limit()
@@ -31,45 +90,28 @@ def process_event(automation_data, outputs):
         resources = chunk["entities"]
         automation = chunk["automation"]
         automation_name = automation["name"]
+        action_params = automation.get("params", {})
+
+        # Calc pool size
+        max_pool_size = 20
+        pool_size = min(len(resources), max_pool_size)
+
+        # Divide the resources into batches for full pool utilisation
+        resource_batches = (resources[i:i + pool_size] for i in range(0, len(resources), pool_size))
+
+        # Mix in other args to supply to the worker func
+        batches_args = ([resource_batch, automation_name, action_params] for resource_batch in resource_batches)
+
+        # Run work on process pool, blocks until complete
+        with mp.Pool(processes=pool_size) as pool:
+            results = pool.starmap(worker, batches_args, 1)
 
         ## Augment the automation dict to track errors and add to the output, this gets reported back to Hyperglance
-        automation["processed"] = []
-        automation["errored"] = []
-        automation["critical_error"] = None
+        critical_error_msg = "\n".join((r["critical_error"] for r in results if r["critical_error"] is not None)).strip()
+        automation["critical_error"] = critical_error_msg if critical_error_msg else None
+        automation["processed"] = [r["processed"] for r in results]
+        automation["errored"] = [r["errored"] for r in results]
         outputs.append(automation)
-        ## Dynamically load the module that will handle this automation
-        try:
-            automation_to_execute = importlib.import_module(
-                "".join(["hyperglance_automations.", "actions.", automation_name])
-            )
-        except Exception as e:
-            logger.info(e)
-            msg = "Unable to find or load an automation called: %s" % automation_name
-            automation["critical_error"] = msg
-            return
-
-        ## For each of Resource, execute the automation
-        for resource in resources:
-            if time_elapsed > time_limit:
-                logger.info("time limit exceeded for " + str(resource))
-                resource["error"] = \
-                "The time limit for the action has surpassed. Consider changing your function app service plan. https://docs.microsoft.com/en-us/azure/app-service/app-service-plan-manage"
-                automation['errored'].append(resource)
-                continue
-            before = perf_counter()
-            try:
-                action_params = automation.get("params", {})
-                automation_to_execute.hyperglance_automation(
-                    credential, resource, cloud, action_params
-                )
-                automation["processed"].append(resource)
-
-            except Exception as err:
-                logger.info(err)
-                resource["error"] = str(err)  # augment resource with an error field
-                automation["errored"].append(resource)
-            finally:
-                time_elapsed += (perf_counter()-before)
 
 def get_time_limit():
     host_file = Path(__file__).resolve().parents[0].joinpath('host.json')
