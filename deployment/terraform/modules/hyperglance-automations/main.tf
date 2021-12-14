cc9e46c6db62d61bc4fd6438d8c2f03d5dd64d80
Jake <joneill@hyperglance.com>
2021-12-14 17:32:32 +0000

    Revert "remove application insights"
    This reverts commit 4cb35107dc4c394a99a2284b61253d0415eefff2.

--- deployment/terraform/modules/hyperglance-automations/main.tf
+++ deployment/terraform/modules/hyperglance-automations/main.tf
@@ -70,6 +70,7 @@ resource "azurerm_function_app" "hyperglance-automations-app" {
   app_settings = {
     hyperglanceautomations_STORAGE = azurerm_storage_account.hyperglance-automations-storage-account.primary_connection_string
     AzureWebJobsDisableHomepage    = true
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.hyperglance-automations-application-insights.instrumentation_key
     ENABLE_ORYX_BUILD              = true
     SCM_DO_BUILD_DURING_DEPLOYMENT = 1
     FUNCTIONS_WORKER_RUNTIME       = "python"
@@ -81,6 +82,14 @@ resource "azurerm_function_app" "hyperglance-automations-app" {
   tags = var.tags
 }
 
+
+# Enable application insights for function
+resource "azurerm_application_insights" "hyperglance-automations-application-insights" {
+  name                = random_pet.hyperglance-automations-name.id
+  location            = azurerm_resource_group.hyperglance-automations-resource-group.location
+  resource_group_name = azurerm_resource_group.hyperglance-automations-resource-group.name
+  application_type    = "other"
+}
 
 # Create storage container for Hyperglance function
 resource "azurerm_storage_container" "hyperglance-automations-storage-container" {
