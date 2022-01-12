1737b3bb73cc78fbc1336db1c4d61f3fdf3c6b0a
Jake O'Neill <joneill@hyperglance.com>
2022-01-12 11:53:35 +0000

    create delete resource group automation

---
+++ hyperglance_automations/actions/generic_rg_delete.py
@@ -0,0 +1,24 @@
+def hyperglance_automation(credential, resource: dict, cloud, automation_params = '', **kwargs):
+  from azure.mgmt.resource import ResourceManagementClient
+
+  url = cloud.endpoints.resource_manager
+  client = ResourceManagementClient(credential, resource['subscription'], base_url=url, credential_scopes=[url + '/.default']) # subscription id
+  client.resource_groups.begin_delete(resource['attributes']['Resource Group'])
+
+
+def info() -> dict:
+  INFO = {
+    "displayName": "Delete Resource Group",
+    "description": "Deletes a Resource Group",
+    "resourceTypes": [
+      "Virtual Machine"
+    ],
+    "params": [
+
+    ],
+    "permissions": [ 
+      
+    ]
+  }
+
+  return INFO
